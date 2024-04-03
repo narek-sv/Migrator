d3d6b2f1c40e87f81820aaf0aa03edc40c67d762
narek.sahakyan <narek.sv.work@gmail.com>
2024-04-03 18:20:03 +0400

    Add privacy manifest, change swift from 5.9 to 5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,6 +24,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "MigratorCore",
+                resources: [.process("Resources/PrivacyInfo.xcprivacy")],
                 swiftSettings: [
                     .enableExperimentalFeature("StrictConcurrency"),
                 ]),
